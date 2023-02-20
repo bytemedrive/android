71167fd94eb728dfc60aeab9a69590a6ae072f4a
Ales Spurny <2820662+Blizard@users.noreply.github.com>
2023-02-20 16:40:02 +0100

    File encrypt/decrypt - AES-256-GCM

--- app/src/main/java/com/bytemedrive/MainActivity.kt
+++ app/src/main/java/com/bytemedrive/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.bytemedrive
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bytemedrive.privacy.FileEncrypt
 import com.bytemedrive.ui.theme.ByteMeTheme
 
 class MainActivity : ComponentActivity() {
+    private val salt = "dummmySaltToByte".toByteArray()
+    private val password: String = "dummyPassword"
+
+    private val pickFileLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent(), ::onFilePicked)
+
+    private fun onFilePicked(uri: Uri?) {
+        if (uri != null) {
+            val inputStream = contentResolver.openInputStream(uri)
+
+            if (inputStream != null) {
+                val encryptedFile = FileEncrypt.encryptFile(inputStream.readBytes(), password, salt)
+                // send file to BE
+            }
+
+            inputStream?.close()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ByteMeTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +48,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column() {
+                        Row {
+                            Button(onClick = { pickFileLauncher.launch("*/*") }) {
+                                Text(text = "Pick file")
+                            }
+                        }
+                    }
                 }
             }
         }
