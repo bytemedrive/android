d62766ae85141a82d12264e6201d01897940ca59
Ales Spurny <2820662+Blizard@users.noreply.github.com>
2023-02-20 18:29:16 +0100

    Added navigation bottom bar

---
+++ app/src/main/java/com/bytemedrive/main/MainScreen.kt
@@ -0,0 +1,50 @@
+package com.bytemedrive.main
+
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.navigation.compose.rememberNavController
+import com.bytemedrive.navigation.NavigationBottomBar
+import com.bytemedrive.navigation.NavigationHost
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(pickFileLauncher: ActivityResultLauncher<String>) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "Home",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(onClick = { pickFileLauncher.launch("*/*") }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "fab icon")
+            }
+        },
+        content = { innerPadding ->
+            NavigationHost(navController = navController, innerPadding = innerPadding)
+        },
+        bottomBar = {
+            NavigationBottomBar(navController)
+        }
+    )
+}
---
+++ app/src/main/java/com/bytemedrive/navigation/NavigationBottomBar.kt
@@ -0,0 +1,44 @@
+package com.bytemedrive.navigation
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+fun getNavigationBarItems(actions: MainActions): List<NavigationBarItem> =
+    listOf(
+        NavigationBarItem("Home", "home", Icons.Default.Home, actions.goToHome),
+    )
+
+@Composable
+fun NavigationBottomBar(navController: NavHostController) {
+    val actions = remember(navController) { MainActions(navController) }
+    val navItems = getNavigationBarItems(actions)
+    val selectedIndex = remember { mutableStateOf(navItems[0]) }
+
+    BottomNavigation(elevation = 10.dp, backgroundColor = MaterialTheme.colorScheme.primary) {
+        navItems.forEach { item ->
+            BottomNavigationItem(icon = {
+                Icon(imageVector = item.icon, "")
+            },
+                label = {
+                    Text(
+                        text = item.title,
+                        color = MaterialTheme.colorScheme.inversePrimary
+                    )
+                },
+                selected = (selectedIndex.value == item),
+                onClick = {
+                    selectedIndex.value = item
+                })
+        }
+    }
+}
