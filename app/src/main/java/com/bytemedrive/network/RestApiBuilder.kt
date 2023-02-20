daff87a1150ef8ef3191033c83b7e4c644c379e6
Lukáš Medelský <lukas.medelsky@bsc-ideas.com>
2023-02-20 17:07:10 +0100

    Ktor, Koin, example request

---
+++ app/src/main/java/com/bytemedrive/network/RestApiBuilder.kt
@@ -0,0 +1,54 @@
+package com.bytemedrive.network
+
+import io.earthbanc.mrv.config.ConfigProperty
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.plugin
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class RestApiBuilder() {
+    val client = getHttpClient()
+
+    private fun getHttpClient(): HttpClient {
+        val client = HttpClient(OkHttp) {
+            expectSuccess = true
+
+            install(Logging) {
+                level = LogLevel.ALL
+            }
+
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+
+            defaultRequest {
+                url(ConfigProperty.backendUrl)
+                contentType(ContentType.Application.Json)
+            }
+        }
+
+        client.plugin(HttpSend).intercept { request ->
+            val originalCall = execute(request)
+
+            if (originalCall.response.status.value == 401) {
+                execute(request)
+            } else {
+                originalCall
+            }
+        }
+
+        return client
+    }
+}
