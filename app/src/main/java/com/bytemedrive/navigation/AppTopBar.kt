e9567695a825204d1ddf4e9e03a0fa2eff7ce0e2
Aleš Spurný <2820662+Blizard@users.noreply.github.com>
2023-04-16 11:49:40 +0200

    Sign out (#18)

--- app/src/main/java/com/bytemedrive/navigation/AppTopBar.kt
+++ app/src/main/java/com/bytemedrive/navigation/AppTopBar.kt
@@ -1,21 +1,31 @@
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.bytemedrive.R
+import com.bytemedrive.signin.SignInManager
+import org.koin.androidx.compose.get
 
 @Composable
-fun AppTopBar() {
+fun AppTopBar(signInManager: SignInManager = get()) {
     TopAppBar(
         title = { Text(text = "Top App Bar", color = Color.White) },
         navigationIcon = {
             IconButton(onClick = {}) {
-                Icon(Icons.Filled.ArrowBack, "backIcon", tint = Color.White)
+                Icon(Icons.Filled.ArrowBack, stringResource(id = R.string.common_go_back), tint = Color.White)
+            }
+        },
+        actions = {
+            IconButton(onClick = { signInManager.signOut() }) {
+                Icon(Icons.Filled.Logout, stringResource(id = R.string.common_sign_out), tint = Color.White)
             }
         },
         backgroundColor = MaterialTheme.colorScheme.primary,
