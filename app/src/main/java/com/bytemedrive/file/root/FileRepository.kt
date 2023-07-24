be89e52ca8eb97c6ae583aa26ca04382e690c51c
Antonin Stoklasek <7347733+tonda100@users.noreply.github.com>
2023-07-24 14:38:31 +0200

    WIP (#52)
    Co-authored-by: Github Actions <github.actions@users.noreply.github.com>

--- app/src/main/java/com/bytemedrive/file/root/FileRepository.kt
+++ app/src/main/java/com/bytemedrive/file/root/FileRepository.kt
@@ -1,19 +1,25 @@
 package com.bytemedrive.file.root
 
+import android.util.Log
 import com.bytemedrive.application.httpClient
+import com.bytemedrive.network.RequestFailedException
 import io.ktor.client.request.delete
 import io.ktor.client.request.forms.InputProvider
 import io.ktor.client.request.forms.formData
 import io.ktor.client.request.forms.submitFormWithBinaryData
 import io.ktor.client.request.get
 import io.ktor.client.request.header
+import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.readBytes
 import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 import io.ktor.utils.io.streams.asInput
 import java.util.UUID
 
 class FileRepository {
+
+    private val TAG = FileRepository::class.qualifiedName
 
     suspend fun upload(walletId: UUID, chunks: List<Chunk>) {
         chunks.forEach {
@@ -35,7 +41,19 @@ class FileRepository {
         }
     }
 
-    suspend fun download(id: UUID) = httpClient.get("files/$id") { header("Accept", "application/octet-stream") }
+    suspend fun download(id: UUID): HttpResponse? {
+        return try {
+            httpClient.get("files/$id") { header("Accept", "application/octet-stream") }
+        } catch (e: RequestFailedException) {
+            if (e.response.status == HttpStatusCode.NotFound) {
+                Log.w(TAG, "File with id=$id not found")
+                
+                null
+            } else {
+                throw e
+            }
+        }
+    }
 
     suspend fun remove(walletId: UUID, id: UUID) = httpClient.delete("wallets/$walletId/files/$id")
 }
