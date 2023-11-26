c1479512856fc31d353bac271950a40f87c955b7
Lukáš Medelský <12560054+k3nda@users.noreply.github.com>
2023-11-26 11:29:51 +0100

    Thumbnail service - download (#117)

--- app/src/main/java/com/bytemedrive/service/ServiceManager.kt
+++ app/src/main/java/com/bytemedrive/service/ServiceManager.kt
@@ -9,11 +9,13 @@ class ServiceManager {
         context.startService(Intent(context, ServiceFileUpload::class.java))
         context.startService(Intent(context, ServiceFileDownload::class.java))
         context.startService(Intent(context, ServiceThumbnailCreate::class.java))
+        context.startService(Intent(context, ServiceThumbnailDownload::class.java))
     }
 
     fun stopServices(context: Context) {
         context.stopService(Intent(context, ServiceFileUpload::class.java))
         context.stopService(Intent(context, ServiceFileDownload::class.java))
         context.stopService(Intent(context, ServiceThumbnailCreate::class.java))
+        context.stopService(Intent(context, ServiceThumbnailDownload::class.java))
     }
 }
