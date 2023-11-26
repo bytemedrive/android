6a40bf1387340fd8a0ffe94c17be320c1fe178a3
Ales Spurny <2820662+Blizard@users.noreply.github.com>
2023-11-26 16:28:40 +0100

    Field password eye protection - show/hide on focus

--- app/src/main/java/com/bytemedrive/ui/component/FieldPassword.kt
+++ app/src/main/java/com/bytemedrive/ui/component/FieldPassword.kt
@@ -1,5 +1,7 @@
 package com.bytemedrive.ui.component
 
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -20,6 +22,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.autofill.AutofillType
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.PointerInputScope
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -47,18 +53,47 @@ fun FieldPassword(
             val visibilityIcon = if (passwordVisibility) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
 
             if (showEye == true) {
-                IconButton(
-                    onClick = { passwordVisibility = !passwordVisibility },
-                    content = { Icon(visibilityIcon, contentDescription = if (passwordVisibility) "Hide password" else "Show password") }
-                )
+                Box(
+                    modifier = Modifier
+                        .pointerInput(Unit) {
+                            routePointerChangesTo(
+                                onDown = { _ -> passwordVisibility = true },
+                                onUp = { _ -> passwordVisibility = false }
+                            )
+                        }
+                ) {
+                    Icon(visibilityIcon, contentDescription = if (passwordVisibility) "Hide password" else "Show password")
+                }
             }
         },
+        singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardOptions.capitalization, keyboardOptions.autoCorrect, KeyboardType.Password, keyboardOptions.imeAction),
         keyboardActions = keyboardActions,
         modifier = modifier
             .autofill(
                 autofillTypes = listOf(AutofillType.Password),
                 onFill = { onValueChange(it.toCharArray()) },
-            ).focusRequester(focusRequester),
+            )
+            .focusRequester(focusRequester),
     )
+}
+
+private suspend fun PointerInputScope.routePointerChangesTo(
+    onDown: (PointerInputChange) -> Unit = {},
+    onMove: (PointerInputChange) -> Unit = {},
+    onUp: (PointerInputChange) -> Unit = {}
+) {
+    awaitEachGesture {
+        do {
+            val event = awaitPointerEvent()
+            event.changes.forEach {
+                when (event.type) {
+                    PointerEventType.Press -> onDown(it)
+                    PointerEventType.Move -> onMove(it)
+                    PointerEventType.Release -> onUp(it)
+                }
+                it.consume()
+            }
+        } while (event.changes.any { it.pressed })
+    }
 }
