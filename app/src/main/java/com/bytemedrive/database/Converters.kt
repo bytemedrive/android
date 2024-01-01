391dd9f4fc20af14bd7c673680fe58cf831a6204
Antonin Stoklasek <7347733+tonda100@users.noreply.github.com>
2024-01-01 19:13:47 +0100

    added converters for enums

--- app/src/main/java/com/bytemedrive/database/Converters.kt
+++ app/src/main/java/com/bytemedrive/database/Converters.kt
@@ -2,8 +2,10 @@ package com.bytemedrive.database
 
 import androidx.room.TypeConverter
 import com.bytemedrive.datafile.entity.Thumbnail
+import com.bytemedrive.datafile.entity.UploadStatus
 import com.bytemedrive.file.root.UploadChunk
 import com.bytemedrive.network.JsonConfig.mapper
+import com.bytemedrive.store.EventType
 import com.fasterxml.jackson.core.type.TypeReference
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -11,12 +13,24 @@ import java.time.format.DateTimeFormatter
 class Converters {
 
     @TypeConverter
-    fun convertZonedDateTimeSerializerToString(zonedDateTime: ZonedDateTime?): String? = zonedDateTime?.format(DateTimeFormatter.ISO_ZONED_DATE_TIME)
+    fun convertZonedDateTime(zonedDateTime: ZonedDateTime?): String? = zonedDateTime?.format(DateTimeFormatter.ISO_ZONED_DATE_TIME)
 
     @TypeConverter
-    fun convertStringToZonedDateTimeSerializer(value: String?): ZonedDateTime? = value?.let {
+    fun convertZonedDateTime(value: String?): ZonedDateTime? = value?.let {
         ZonedDateTime.parse(value, DateTimeFormatter.ISO_ZONED_DATE_TIME)
     }
+
+    @TypeConverter
+    fun convertUploadStatus(value: String): UploadStatus = UploadStatus.valueOf(value)
+
+    @TypeConverter
+    fun convertUploadStatus(value: UploadStatus): String = value.name
+
+    @TypeConverter
+    fun convertEventType(value: String): EventType = EventType.valueOf(value)
+
+    @TypeConverter
+    fun convertEventType(value: EventType): String = value.name
 
     @TypeConverter
     fun convertUploadChunks(data: List<UploadChunk>): String = mapper.writeValueAsString(data)
