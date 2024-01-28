b8371b2c517ef70364bfb9728d9f7b0071902920
Antonin Stoklasek <7347733+tonda100@users.noreply.github.com>
2024-01-28 15:52:55 +0100

    set default date format for json deser

--- app/src/main/java/com/bytemedrive/network/JsonConfig.kt
+++ app/src/main/java/com/bytemedrive/network/JsonConfig.kt
@@ -1,12 +1,13 @@
 package com.bytemedrive.network
 
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
+import java.text.SimpleDateFormat
+import java.util.TimeZone
 
 object JsonConfig {
 
@@ -18,10 +19,13 @@ object JsonConfig {
             registerModule(kotlinModule())
             configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-            configure(SerializationFeature.INDENT_OUTPUT, true)
-            setDefaultPrettyPrinter(DefaultPrettyPrinter().apply {
-                indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
-            })
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            setTimeZone(TimeZone.getTimeZone("UTC"))
+            setDateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ"))
+            //configure(SerializationFeature.INDENT_OUTPUT, true)
+            //setDefaultPrettyPrinter(DefaultPrettyPrinter().apply {
+            //    indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
+            //})
         }
     }
 }
