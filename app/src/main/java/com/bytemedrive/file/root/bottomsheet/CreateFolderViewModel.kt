1ccdcf33eaa381f8df48d8c3e154900ca7dcdc0c
Ales Spurny <2820662+Blizard@users.noreply.github.com>
2024-02-03 15:51:25 +0100

    Clear folder name when cancel create folder dialog

--- app/src/main/java/com/bytemedrive/file/root/bottomsheet/CreateFolderViewModel.kt
+++ app/src/main/java/com/bytemedrive/file/root/bottomsheet/CreateFolderViewModel.kt
@@ -1,5 +1,8 @@
 package com.bytemedrive.file.root.bottomsheet
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bytemedrive.folder.EventFolderCreated
@@ -14,9 +17,9 @@ class CreateFolderViewModel(
     private val eventPublisher: EventPublisher
 ) : ViewModel() {
 
-    val name = MutableStateFlow("")
+    var name by mutableStateOf("")
 
     fun createFolder(folderId: UUID?) = externalScope.launch {
-        eventPublisher.publishEvent(EventFolderCreated(UUID.randomUUID(), name.value, parent = folderId))
+        eventPublisher.publishEvent(EventFolderCreated(UUID.randomUUID(), name, parent = folderId))
     }
 }
