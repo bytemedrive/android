c216e7e6df39cc4a7af803874fcdd9c97a42a751
Aleš Spurný <2820662+Blizard@users.noreply.github.com>
2024-02-05 12:24:48 +0100

    Removed throwing exception that stopped uploading rest of files in queue (#194)

--- app/src/main/java/com/bytemedrive/service/ServiceFileUpload.kt
+++ app/src/main/java/com/bytemedrive/service/ServiceFileUpload.kt
@@ -77,8 +77,6 @@ class ServiceFileUpload : Service() {
             } catch (exception: Exception) {
                 Log.e(TAG, "File upload failed! File path=${file.path}.")
                 queueFileUploadRepository.deleteFile(fileUpload.id)
-
-                throw IllegalStateException("${file.name} upload failed! Please try again.", exception)
             }
         } else {
             Log.w(TAG, "File upload canceled. File ${file.path} could not be found.")
