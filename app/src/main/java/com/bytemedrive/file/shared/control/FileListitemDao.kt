e15a919d6d94a6b3ca196647ee5713c26d7c0b40
Ales Spurny <2820662+Blizard@users.noreply.github.com>
2024-03-17 10:43:18 +0100

    PR fixes

--- app/src/main/java/com/bytemedrive/file/shared/control/FileListitemDao.kt
+++ app/src/main/java/com/bytemedrive/file/shared/control/FileListitemDao.kt
@@ -7,17 +7,37 @@ import com.bytemedrive.file.shared.entity.FileListItemEntity
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
-const val queryAll = """
-        SELECT id, name, 'FOLDER' AS type, starred, 'COMPLETED' AS uploadStatus, parent AS folderId 
-        FROM folder 
+@Dao
+interface FileListItemDao {
+
+    @Query(
+        """
+        SELECT id, name, 'FOLDER' AS type, starred, 'COMPLETED' AS uploadStatus, parent AS folderId
+        FROM folder
         WHERE parent IS :folderId
         UNION ALL
-            SELECT id, name, 'FILE' AS type, starred, uploadStatus, folderId 
-            FROM data_file_link 
-            WHERE folderId IS :folderId
+        SELECT id, name, 'FILE' AS type, starred, uploadStatus, folderId
+        FROM data_file_link
+        WHERE folderId IS :folderId
         """
+    )
+    suspend fun getAll(folderId: UUID?): List<FileListItemEntity>
 
-const val queryStarredAll = """
+    @Query(
+        """
+        SELECT id, name, 'FOLDER' AS type, starred, 'COMPLETED' AS uploadStatus, parent AS folderId
+        FROM folder
+        WHERE parent IS :folderId
+        UNION ALL
+        SELECT id, name, 'FILE' AS type, starred, uploadStatus, folderId
+        FROM data_file_link
+        WHERE folderId IS :folderId
+        """
+    )
+    fun getAllPaged(folderId: UUID?): PagingSource<Int, FileListItemEntity>
+
+    @Query(
+        """
         SELECT id, name, 'FOLDER' AS type, starred, 'COMPLETED' AS uploadStatus, parent AS folderId 
         FROM folder 
         WHERE starred = :starred
@@ -26,19 +46,19 @@ const val queryStarredAll = """
             FROM data_file_link 
             WHERE starred = :starred
         """
-
-@Dao
-interface FileListItemDao {
-
-    @Query(queryAll)
-    suspend fun getAll(folderId: UUID?): List<FileListItemEntity>
-
-    @Query(queryAll)
-    fun getAllPaged(folderId: UUID?): PagingSource<Int, FileListItemEntity>
-
-    @Query(queryStarredAll)
+    )
     fun getAllStarredPaged(starred: Boolean): PagingSource<Int, FileListItemEntity>
 
-    @Query(queryStarredAll)
+    @Query(
+        """
+        SELECT id, name, 'FOLDER' AS type, starred, 'COMPLETED' AS uploadStatus, parent AS folderId 
+        FROM folder 
+        WHERE starred = :starred
+        UNION ALL
+            SELECT id, name, 'FILE' AS type, starred, uploadStatus, folderId 
+            FROM data_file_link 
+            WHERE starred = :starred
+        """
+    )
     fun getAllStarredFlow(starred: Boolean): Flow<List<FileListItemEntity>>
 }
